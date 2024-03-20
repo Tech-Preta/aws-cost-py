2d6da1d697de566276fe45f68f6b898ccaffa8ed
nataliagranato <natalia.granato@jackexperts.com>
2024-03-20 11:57:15 -0300

    feat: tests and __init__.py

--- __init__.py
+++ __init__.py
@@ -1,9 +1,16 @@
-from analise_de_custos import get_custom_date_range, list_costs_by_service
+"""
+Este é o módulo principal do projeto aws_cost_py.
+Ele importa e executa as funções do módulo analise_de_custos.
+"""
+
+from .analise_de_custos import get_custom_date_range, list_costs_by_service
 
 def main():
+    """
+    Função principal que solicita ao usuário um intervalo de datas e lista os custos dos serviços AWS para esse intervalo.
+    """
     start_date, end_date = get_custom_date_range()
-    if start_date and end_date:
-        list_costs_by_service(start_date, end_date)
+    list_costs_by_service(start_date, end_date)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- tests/test_analise_de_custos.py
+++ tests/test_analise_de_custos.py
@@ -1,6 +1,18 @@
+"""
+Este é o módulo de teste para o módulo analise_de_custos.
+Ele contém testes unitários para as funções get_custom_date_range e list_costs_by_service.
+"""
+
 import unittest
-from analise_de_custos import get_custom_date_range, list_costs_by_service
+from analise_de_custos import get_custom_date_range
+
 class TestAnaliseDeCustos(unittest.TestCase):
+    """
+    Esta classe contém testes unitários para o módulo analise_de_custos.
+    """
 
     def test_get_custom_date_range_valid_input(self):
-        start_date, end_date = get_custom_date_range()
+        """
+        Testa a função get_custom_date_range com uma entrada válida.
+        """
+        input_start_date, input_end_date = get_custom_date_range()
